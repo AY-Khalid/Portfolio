1b9ef76c30f5394801b5174c60f0296c51face2d
AYKhalid <aniduyakubu@yahoo.com>
2022-07-02 12:34:06 +0100

    second

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-canvas.addEventListener('mousemove', function (event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    hue += 2;
-    if (frame % 2 === 0) {
-        for (let i = 0; i < 7; i++) {
-            particlesArray.push(new Particle());
-        }
-    }
-});
+function showLanguage() {
+    let languageItems = document.getElementById("language-items");
+    languageItems.style.visibility = "visible";
+}
